b4b400fce6785da3e8703fab475396b9b2a1b7e9
Hamir Dhanquer C. Noleto <hamir.noleto@fundag.local>
2020-02-07 18:17:04 -0200

    Atualizando projeto Node

--- Exercicios_web/vue/calculator/src/main/Calculator.vue
+++ Exercicios_web/vue/calculator/src/main/Calculator.vue
@@ -2,24 +2,24 @@
   
     <div class="calculator">
 
-        <Display value="1000" ></Display>
-        <Button label="AC" triple ></Button>
-        <Button label="/" operation ></Button>
-        <Button label="7" ></Button>
-        <Button label="8" ></Button>
-        <Button label="9" ></Button>
-        <Button label="*" operation ></Button>
-        <Button label="4" ></Button>
-        <Button label="5" ></Button>
-        <Button label="6" ></Button>
-        <Button label="-" operation></Button>
-        <Button label="1" ></Button>
-        <Button label="2" ></Button>
-        <Button label="3" ></Button>
-        <Button label="+" operation></Button>
-        <Button label="0" double></Button>
-        <Button label="." ></Button>
-        <Button label="=" operation></Button>
+        <Display :value="displayValue" ></Display>
+        <Button label="AC" triple @onClick="clearMemory"></Button>
+        <Button label="/" operation @onClick="setOperation"></Button>
+        <Button label="7" @onClick="addDigit"></Button>
+        <Button label="8" @onClick="addDigit"></Button>
+        <Button label="9" @onClick="addDigit"></Button>
+        <Button label="*" operation @onClick="setOperation"></Button>
+        <Button label="4" @onClick="addDigit"></Button>
+        <Button label="5" @onClick="addDigit"></Button>
+        <Button label="6" @onClick="addDigit"></Button>
+        <Button label="-" operation @onClick="setOperation"></Button>
+        <Button label="1" @onClick="addDigit"></Button>
+        <Button label="2" @onClick="addDigit"></Button>
+        <Button label="3" @onClick="addDigit"></Button>
+        <Button label="+" operation @onClick="setOperation"></Button>
+        <Button label="0" double @onClick="addDigit"></Button>
+        <Button label="." @onClick="addDigit"></Button>
+        <Button label="=" operation @onClick="setOperation"></Button>
     </div>
 
 </template>
@@ -29,9 +29,63 @@ import Display from "../components/Display"
 import Button from "../components/Button"
 
 export default {
-    components: { Button, Display }
+    data: function(){
+        return {
+            displayValue: "0",
+            clearDisplay: false,
+            operation: null,
+            values: [0,0],
+            current: 0
+        }
+    },
+    components: { Button, Display },
+    methods:{
+        clearMemory(){
+            Object.assign(this.$data, this.$options.data())
+        },
+        setOperation(operation){
+           if(this.current === 0){
+               this.operation = operation
+               this.current = 1
+               this.clearDisplay = true
+           }else{
+               const equals = operation === "="
+               const currentOperation = this.operation
+               try{
+                   this.values[0] = eval(
+                       `${this.values[0]}  ${currentOperation} ${this.values[1]}`
+                   )
+               } catch(e){
+                   this.$emit('onError',e)
+               }
+               this.values[1] = 0
+               this.displayValue = this.values[0]
+               this.operation = equals ? null : operation
+               this.current = equals ? 0 : 1
+               this.clearDisplay = !equals
+           }
+        },
+        addDigit(n){
+            if(n === "." && this.displayValue.includes(".")){
+                return
+            }
 
+            const clearDisplay = this.displayValue === "0"
+                    || this.clearDisplay
+                const currentValue = clearDisplay ? "" : this.displayValue
+                const displayValue = currentValue + n 
 
+               this.displayValue = displayValue
+               this.clearDisplay = false
+
+               if(n !== "."){
+                   const i = this.current 
+                   const newValue = parseFloat(displayValue)
+                   this.values[i] = newValue 
+               }
+
+        }
+    }
 }
 </script>
 
