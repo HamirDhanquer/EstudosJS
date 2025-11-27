c48809796477acefb7c3d33cc2ea6f67be575410
Hamir Dhanquer <hamir@fundag.br>
2025-11-26 22:28:14 -0300

    Estudo TS 01

---
+++ Ts_project01/src/helpers/TS_03_IMPLICITA.ts
@@ -0,0 +1,13 @@
+let nyName = 'Alice';
+let age = 30;
+let isAdmin = false;
+
+let person = {
+    name: 'Hamir',
+    age: 40,
+    isAdmin: false
+};
+
+
+
+console.log(person);
---
+++ Ts_project01/src/helpers/TS_04_EXPLICITO.ts
@@ -0,0 +1,12 @@
+let myName: string;
+myName = 'Alice Doe';
+
+let age: number = 28;
+
+let isAdmin: boolean = true;
+
+let Permission: boolean | string | number;
+Permission = false;
+
+
+console.log(Permission);
